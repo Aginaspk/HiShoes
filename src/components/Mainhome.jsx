385a39dd5ddb3c8998e5ee07dfc1f8462a193d4e
Aginas pk <aginaspk6@gmail.com>
2024-11-29 10:26:28 +0530

    Update project with new changes

--- src/components/Mainhome.jsx
+++ src/components/Mainhome.jsx
@@ -5,26 +5,26 @@ import nike from '../images/nike.png'
 function Mainhome() {
     return (
         <>
-            <div className='flex max-sm:flex-col-reverse'>
-                <div className='w-1/2 h-screen max-sm:w-full max-sm:h-[70vh] '>
-                    <div className='mt-72 ml-20 max-sm:mt-36 max-sm:ml-10'>
-                        <div className='w-[500px] max-sm:w-[400px]'>
-                            <h1 className='h1 text-5xl text-center'>Nike Court Vision Low Next Nature</h1>
+            <div className='flex flex-col relative 2xl:static 2xl:flex-row'>
+                <div className='w-full h-[50vh] absolute z-10 top-20 2xl:static 2xl:w-1/2 2xl:h-screen 2xl:z-0'>
+                    <div className=''>
+                        <div className='w-full'>
+                            <h1 className='h1 text-4xl text-center'>Nike Court Vision Low Next Nature</h1>
                         </div>
-                        <p className='p ml-4 text-xl mt-10'>MRP : ₹4,999.00</p>
+                        <p className='p text-xl ml-3 mt-4'>MRP : ₹4,999.00</p>
                     </div>
-                    <div className='text-xl flex ml-24 mt-5 max-sm:ml-12'>
+                    <div className='text-xl flex ml-3 mt-4'>
                         <i><FontAwesomeIcon icon={faGenderless} className='mr-1' /></i>
                         <p className='mr-10'>Men</p>
                         <i><FontAwesomeIcon icon={faTape} className='mr-1' /></i>
                         <p>7,8,9,10,11</p>
 
                     </div>
-                    <div className='text-xl ml-60 mt-10 max-sm:ml-32'>
-                        <button className='bg-[#e3ddd8] px-20 py-3 rounded-lg'>Details</button>
+                    <div className='text-xl mt-64 ml-24'>
+                        <button className='bg-[#e3ddd8] rounded-lg py-5 px-20'>Details</button>
                     </div>
                 </div>
-                <div className='w-1/2 h-screen max-sm:w-full max-sm:h-[70vh]'>
+                <div className='w-full h-[70vh] absolute z-0 2xl:static 2xl:w-1/2 2xl:h-screen'>
                     <img src={nike} alt="" className='h-full w-full object-cover' />
                 </div>
             </div>
